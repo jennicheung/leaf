194013e86d661647436b0d3aa7c21265af27ce4d
jennicheung <jennicheung@gmail.com>
2021-03-31 12:50:25 -0700

    setup

---
+++ src/components/Main.vue
@@ -0,0 +1,76 @@
+<template>
+  <form @submit="submitQuery" action="https://vuejs.org/" method="post">
+    <label for="query">Search Term</label>
+    <input id="query" v-model="query" type="text" name="query" />
+    <input type="submit" value="Submit" />
+  </form>
+</template>
+
+<script>
+import testData from "../testData.js";
+
+/* eslint-disable no-debugger */
+
+export default {
+  name: "Main",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      info: null,
+      query: "tesla",
+      data: [],
+    };
+  },
+  methods: {
+    processData: function(data) {
+      this.data = data.articles;
+      console.log(this.data[0]);
+    },
+    queryTerm: function(term) {
+      console.log(term);
+      this.processData(testData);
+
+      /*
+      fetch(
+        `https://newsapi.org/v2/everything?pageSize=20&q=${encodeURI(
+          term
+        )}&apiKey=3db209faf53643f79427e8b2d2345f6b`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.processData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+        */
+    },
+    submitQuery: function(e) {
+      this.queryTerm(this.query);
+      e.preventDefault();
+    },
+  },
+  mounted() {},
+};
+/* eslint-enable no-debugger */
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/testData.js
@@ -0,0 +1,289 @@
+export default {
+  status: "ok",
+  totalResults: 8142,
+  articles: [
+    {
+      source: { id: null, name: "Lifehacker.com" },
+      author: "David Murphy",
+      title: "What You Need to Know About Buying Cryptocurrency on PayPal",
+      description:
+        "Whether you’re looking to make a larger investment or you just want to dabble in cryptocurrencies, you can purchase Bitcoin, Ethereum, Bitcoin Cash, and Litecoin through PayPal. And, soon, you’ll be able to pay various merchants using your cryptocurrency stas…",
+      url:
+        "https://lifehacker.com/what-you-need-to-know-about-buying-cryptocurrency-on-pa-1846585705",
+      urlToImage:
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/qvc2foo4ufow1cbsuk9f.jpg",
+      publishedAt: "2021-03-31T14:00:00Z",
+      content:
+        "Whether youre looking to make a larger investment or you just want to dabble in cryptocurrencies, you can purchase Bitcoin, Ethereum, Bitcoin Cash, and Litecoin through PayPal. And, soon, youll be ab… [+3818 chars]",
+    },
+    {
+      source: { id: "engadget", name: "Engadget" },
+      author: "https://www.engadget.com/about/editors/steve-dent",
+      title: "Tesla now accepts Bitcoin in the US",
+      description:
+        "As it promised earlier this year, Tesla now accepts payment in Bitcoin, according to Tesla's website and a tweet from CEO Elon Musk.",
+      url:
+        "https://www.engadget.com/tesla-will-soon-let-you-buy-one-of-its-e-vs-with-bitcoin-082916783.html",
+      urlToImage:
+        "https://s.yimg.com/os/creatr-uploaded-images/2021-03/d3bc78e0-8116-11eb-87fb-7bddf8711d48",
+      publishedAt: "2021-03-24T08:29:16Z",
+      content:
+        "As it promised earlier this year, Tesla now accepts payment in Bitcoin, according to Tesla's website and a tweet from CEO Elon Musk. In a subsequent tweet, Musk said that \"Bitcoin paid to Tesla will … [+1390 chars]",
+    },
+    {
+      source: { id: "mashable", name: "Mashable" },
+      author: "Stan Schroeder",
+      title: "You can now buy a Tesla with Bitcoin, Elon Musk says",
+      description:
+        "The inevitable has happened: You can now purchase a Tesla vehicle with Bitcoin. \nThis is according to Tesla CEO, pardon, Technoking Elon Musk, who tweeted it on Wednesday. \n\nYou can now buy a Tesla with Bitcoin\r\n\n— Elon Musk (@elonmusk) March 24, 2021\r\n\n\r\n\nTe…",
+      url: "https://mashable.com/article/elon-musk-tesla-bitcoin/",
+      urlToImage:
+        "https://mondrian.mashable.com/2021%252F03%252F24%252F4c%252F5e557a4d5d144e0898e8f087466d455a.46b57.jpg%252F1200x630.jpg?signature=O7ZjxA7YXNEMQMd0_p1tOsVA6iY=",
+      publishedAt: "2021-03-24T08:10:09Z",
+      content:
+        "The inevitable has happened: You can now purchase a Tesla vehicle with Bitcoin. \r\nThis is according to Tesla CEO, pardon, Technoking Elon Musk, who tweeted it on Wednesday. \r\nYou can now buy a Tesla … [+1648 chars]",
+    },
+    {
+      source: { id: "techcrunch", name: "TechCrunch" },
+      author: "Darrell Etherington",
+      title:
+        "Elon Musk declares you can now buy a Tesla with Bitcoin in the U.S.",
+      description:
+        "Tesla made headlines earlier this year when it took out significant holdings in bitcoin, acquiring a roughly $1.5 billion stake at then-prices in early February. At the time, it also noted in an SEC filing disclosing the transaction that it could also eventua…",
+      url:
+        "http://techcrunch.com/2021/03/24/elon-musk-declares-you-can-now-buy-a-tesla-with-bitcoin-in-the-u-s/",
+      urlToImage:
+        "https://techcrunch.com/wp-content/uploads/2021/02/tesla-bitcoin-alt.jpg?w=711",
+      publishedAt: "2021-03-24T11:55:49Z",
+      content:
+        "Tesla made headlines earlier this year when it took out significant holdings in bitcoin, acquiring a roughly $1.5 billion stake at then-prices in early February. At the time, it also noted in an SEC … [+2071 chars]",
+    },
+    {
+      source: { id: null, name: "Lifehacker.com" },
+      author: "Aisha Jordan",
+      title: "Where to Get Free and Cheap Burritos for National Burrito Day",
+      description:
+        "National Burrito Day lands on April Fool’s Day this year, but thankfully restaurants aren’t playing around with their deals. Starting tomorrow, restaurants like Pollo Loco and Moe’s Southwest Grill are offering buy-one-get-one deals, $5 burritos, and more. Le…",
+      url:
+        "https://lifehacker.com/where-to-get-free-and-cheap-burritos-for-national-burri-1846590609",
+      urlToImage:
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/utvzrithrafokwbcktzd.jpg",
+      publishedAt: "2021-03-31T18:30:00Z",
+      content:
+        "National Burrito Day lands on April Fools Day this year, but thankfully restaurants arent playing around with their deals. Starting tomorrow, restaurants like Pollo Loco and Moes Southwest Grill are … [+2882 chars]",
+    },
+    {
+      source: { id: "the-verge", name: "The Verge" },
+      author: "Adi Robertson",
+      title:
+        "India will reportedly introduce bill to make owning cryptocurrency illegal",
+      description:
+        "India’s legislature is reportedly considering a near-total ban on private cryptocurrencies like Bitcoin or Ethereum, including owning the virtual currency. The government has discussed plans for a national digital currency as an alternative.",
+      url:
+        "https://www.theverge.com/2021/3/15/22332677/india-cryptocurrency-trading-mining-possession-ban-law-report",
+      urlToImage:
+        "https://cdn.vox-cdn.com/thumbor/IdgNJaOIQBsN8QbQcH2MDU6sAUA=/0x243:2040x1311/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/10432811/mdoying_180308_2373_0091still.jpg",
+      publishedAt: "2021-03-15T22:25:02Z",
+      content:
+        "One of the strictest crackdowns worldwide\r\nPhoto by Michele Doying / The Verge\r\nIndia is reportedly moving forward with a sweeping ban on cryptocurrencies. According to Reuters, the countrys legislat… [+1656 chars]",
+    },
+    {
+      source: { id: "mashable", name: "Mashable" },
+      author: "(Deleted account)",
+      title:
+        "Teen hacker behind the big Twitter hack sentenced to 3 years in prison",
+      description:
+        "The hacker behind last year’s big Twitter hack\r\n has just been sentenced to hard time.\nGraham Ivan Clark, the teenage hacker who broke\r\n into Twitter’s systems, took over verified accounts, and used them to scam users out of Bitcoin, was sentenced to three ye…",
+      url: "https://mashable.com/article/teen-twitter-hacker-prison/",
+      urlToImage:
+        "https://mondrian.mashable.com/2021%252F03%252F16%252Fde%252F17c7b97375614eff9a3101c524b8bdc7.3ddd9.jpg%252F1200x630.jpg?signature=hVkjCyubP0Ak6Mnye3LA3kpUeCE=",
+      publishedAt: "2021-03-16T21:40:35Z",
+      content:
+        "The hacker behind last years big Twitter hack\r\n has just been sentenced to hard time.\r\nGraham Ivan Clark, the teenage hacker who broke\r\n into Twitters systems, took over verified accounts, and used t… [+2552 chars]",
+    },
+    {
+      source: { id: null, name: "Lifehacker.com" },
+      author: "David Murphy",
+      title: "How to Create NFTs for Fun and Profit, Maybe",
+      description:
+        "It seems like everyone is making money off of NFTs, or non-fungible tokens, those digital artworks that have been labeled the next investment mania. Everyone, that is, except for you. It’s the same feeling you had when you realized GameStop’s stock was a thin…",
+      url:
+        "https://lifehacker.com/how-to-create-nfts-for-fun-and-profit-maybe-1846562554",
+      urlToImage:
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/ehfmb5iugt8xbd2fklol.png",
+      publishedAt: "2021-03-29T13:30:00Z",
+      content:
+        "It seems like everyone is making money off of NFTs, or non-fungible tokens, those digital artworks that have been labeled the next investment mania. Everyone, that is, except for you. Its the same fe… [+5036 chars]",
+    },
+    {
+      source: { id: "cnn", name: "CNN" },
+      author: null,
+      title: "Why bitcoin could triple over next year",
+      description:
+        "William Quigley, managing director of Magnetic, explains why he believes that bitcoin's value can still rise dramatically into 2022.",
+      url:
+        "https://www.cnn.com/videos/business/2021/03/03/bitcoin-crypto-william-quigley-markets-now.cnn-business",
+      urlToImage:
+        "https://cdn.cnn.com/cnnnext/dam/assets/210219081655-02-bitcoin-file-super-tease.jpg",
+      publishedAt: "2021-03-03T19:10:57Z",
+      content: "",
+    },
+    {
+      source: { id: "techcrunch", name: "TechCrunch" },
+      author: "Jonathan Shieber",
+      title:
+        "Spinning out from the cryptocurrency hardware developer Bitfury, LiquidStack pitches a data center cooling tech",
+      description:
+        "Data centers and bitcoin mining operations are becoming huge energy hogs and the explosive growth of both risks undoing a lot of the progress that’s been made to reduce global greenhouse gas emissions. It’s one of the major criticisms of cryptocurrency operat…",
+      url:
+        "http://techcrunch.com/2021/03/25/spinning-out-from-the-cryptocurrency-hardware-developer-bitfury-liquidstack-pitches-a-data-center-cooling-tech/",
+      urlToImage:
+        "https://techcrunch.com/wp-content/uploads/2021/03/GettyImages-1230723417.jpg?w=600",
+      publishedAt: "2021-03-25T13:30:01Z",
+      content:
+        "Data centers and bitcoin mining operations are becoming huge energy hogs and the explosive growth of both risks undoing a lot of the progress that’s been made to reduce global greenhouse gas emission… [+2494 chars]",
+    },
+    {
+      source: { id: null, name: "Gizmodo.com" },
+      author: "Matt Novak",
+      title:
+        "India Looks to Criminalize Trading, Mining, and Holding Cryptocurrencies: Report",
+      description:
+        "A proposed law in India would make it a crime to mine, trade, or even hold cryptocurrencies like bitcoin in the country, according to a new report from Reuters citing a senior government official. It’s estimated roughly 8 million people in India hold some for…",
+      url:
+        "https://gizmodo.com/india-looks-to-criminalize-trading-mining-and-holding-1846475775",
+      urlToImage:
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/uvoltaxovjhfhb5x5ozr.jpg",
+      publishedAt: "2021-03-15T09:15:00Z",
+      content:
+        "A proposed law in India would make it a crime to mine, trade, or even hold cryptocurrencies like bitcoin in the country, according to a new report from Reuters citing a senior government official. It… [+1725 chars]",
+    },
+    {
+      source: { id: "techcrunch", name: "TechCrunch" },
+      author: "Sarah Perez",
+      title:
+        "PayPal’s new feature allows U.S. consumers to check out using cryptocurrency",
+      description:
+        "PayPal this morning announced the launch of Checkout with Crypto, a new feature that will allow consumers to check out at millions of online businesses using cryptocurrency. The feature expands on PayPal’s current investments in the cryptocurrency market, whi…",
+      url:
+        "http://techcrunch.com/2021/03/30/paypals-new-feature-allows-u-s-consumers-to-check-out-using-cryptocurrency/",
+      urlToImage:
+        "https://techcrunch.com/wp-content/uploads/2021/03/CheckoutCrypto_THUMBNAIL.jpg?w=598",
+      publishedAt: "2021-03-30T13:54:47Z",
+      content:
+        "PayPal this morning announced the launch of Checkout with Crypto, a new feature that will allow consumers to check out at millions of online businesses using cryptocurrency. The feature expands on Pa… [+3997 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Staff",
+      title: "MicroStrategy buys 205 bitcoin for $10 million - Reuters",
+      description:
+        "MicroStrategy Inc, a major corporate backer of bitcoin, has bought about 205 bitcoin for about $10 million in cash, it said on Friday, adding to its already substantial holdings of the cryptocurrency.",
+      url:
+        "https://www.reuters.com/article/us-crypto-currency-microstrategy-idUSKBN2AX1F6",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?m=02&d=20210305&t=2&i=1553866737&r=LYNXMPEH240TQ&w=800",
+      publishedAt: "2021-03-05T13:23:00Z",
+      content:
+        "By Reuters Staff\r\nFILE PHOTO: A collection of bitcoin (virtual currency) tokens are displayed in this picture illustration taken Dec. 8, 2017. REUTERS/Benoit Tessier/Illustration\r\n(Reuters) - MicroSt… [+905 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Staff",
+      title:
+        "MicroStrategy bitcoin holding valued at $4.38 billion after latest buy - Reuters",
+      description:
+        "Major bitcoin corporate backer MicroStrategy Inc said on Monday it bought another 328 bitcoin for about $15 million in cash, swelling its holdings of the cryptocurrency and sending the company's shares 7% higher in premarket trading.",
+      url:
+        "https://www.reuters.com/article/us-crypto-currency-microstrategy-idUSKCN2AT2H5",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?m=02&d=20210301&t=2&i=1553322482&r=LYNXNPEH201G5&w=800",
+      publishedAt: "2021-03-01T13:35:00Z",
+      content:
+        "By Reuters Staff\r\nFILE PHOTO: Representations of virtual currency Bitcoin are placed on a computer motherboard in this illustration taken January 21, 2021. REUTERS/Dado Ruvic/Illustration\r\n(Reuters) … [+1303 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Staff",
+      title: "Breakingviews - Capital Calls: China digs bitcoin hole - Reuters",
+      description:
+        "Breakingviews - Capital Calls: China digs bitcoin hole Reuters",
+      url:
+        "https://www.reuters.com/article/us-global-finance-breakingviews-idUSKCN2AU123",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?m=02&d=20210302&t=2&i=1553439612&r=LYNXNPEH210LQ&w=800",
+      publishedAt: "2021-03-02T10:51:00Z",
+      content:
+        "By Reuters Staff\r\nCryptocurrency mining facilities are seen in Pristina, Kosovo June 12, 2018.\r\nHONG KONG (Reuters Breakingviews) - Concise insights on global finance in the Covid-19 era.\r\nSUPPLY-SID… [+1621 chars]",
+    },
+    {
+      source: { id: null, name: "Entrepreneur" },
+      author: "Entrepreneur en Español",
+      title:
+        "Bitcoin could cost 6 million Mexican pesos, but the fall will be brutal and will last for years, says cryptocurrency expert",
+      description:
+        "Bobby Lee, co-founder of cryptocurrency exchange BTCC, predicts that the Bitcoin 'bubble' will burst after reaching its peak, which is known as 'Bitcoin winter'.",
+      url: "https://www.entrepreneur.com/article/367763",
+      urlToImage:
+        "https://assets.entrepreneur.com/content/3x2/2000/1616529491-Mar23Bitcoinpodrallegaralos300mildlaresPORT.jpeg",
+      publishedAt: "2021-03-23T20:23:00Z",
+      content:
+        "March\r\n23, 2021\r\n3 min read\r\nThis article was translated from our Spanish edition using AI technologies. Errors may exist due to this process.\r\nAfter analyzing the history of the growth Bitcoin , exp… [+2236 chars]",
+    },
+    {
+      source: { id: null, name: "Slashdot.org" },
+      author: "BeauHD",
+      title: "Asset Management Giant Fidelity Files For a Bitcoin ETF",
+      description:
+        "A new filing with the U.S. Securities and Exchange Commission indicates that asset management giant Fidelity is seeking to create a bitcoin exchange-traded fund (ETF). The Block reports: The Wise Origin Bitcoin ETF is the latest entrant in a growing race to l…",
+      url:
+        "https://news.slashdot.org/story/21/03/25/011252/asset-management-giant-fidelity-files-for-a-bitcoin-etf",
+      urlToImage: "https://a.fsdn.com/sd/topics/bitcoin_64.png",
+      publishedAt: "2021-03-25T07:00:00Z",
+      content:
+        "The Wise Origin Bitcoin ETF is the latest entrant in a growing race to launch a bitcoin exchange-traded product in the United States. According to the filing, a firm called FD Funds Management LLC is… [+1081 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Staff",
+      title:
+        "Bitcoin hits record high; trades as high as $59,755 on Saturday - Reuters",
+      description:
+        "Bitcoin, the world's biggest cryptocurrency, on Saturday reached a record high, trading up to $59,755.",
+      url:
+        "https://www.reuters.com/article/us-crypto-currency-bitcoin-idUSKBN2B50B1",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?m=02&d=20210313&t=2&i=1554771186&r=LYNXMPEH2C07M&w=800",
+      publishedAt: "2021-03-13T11:18:00Z",
+      content:
+        "By Reuters Staff\r\nFILE PHOTO: A representation of virtual currency Bitcoin and U.S. One Dollar banknotes are seen in front of a stock graph in this illustration taken January 8, 2021. REUTERS/Dado Ru… [+440 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Staff",
+      title: "Bitcoin hits $60,000 in record high - Reuters",
+      description:
+        "Bitcoin, the world's biggest cryptocurrency, on Saturday crossed a record high of $60,000.",
+      url:
+        "https://www.reuters.com/article/us-crypto-currency-bitcoin-high-idUSKBN2B50BV",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?m=02&d=20210313&t=2&i=1554772780&r=LYNXMPEH2C083&w=800",
+      publishedAt: "2021-03-13T11:54:00Z",
+      content:
+        "By Reuters Staff\r\nFILE PHOTO: Representations of virtual currency Bitcoin and U.S. dollar banknotes are seen in this picture illustration taken January 27, 2020. REUTERS/Dado Ruvic/Illustration\r\n(Reu… [+96 chars]",
+    },
+    {
+      source: { id: "reuters", name: "Reuters" },
+      author: "Reuters Editorial",
+      title: "PayPal starts taking bitcoin at checkout - Reuters.com",
+      description:
+        "PayPal says it has started letting customers use bitcoin at checkout, marking the latest sign that mainstream financial firms are moving to accept cryptocurrencies. Julian Satterthwaite reports.",
+      url: "https://www.reuters.com/video/watch/idOVE6A55E3",
+      urlToImage:
+        "https://static.reuters.com/resources/r/?d=20210330&i=OVE6A55E3&r=OVE6A55E3&t=2",
+      publishedAt: "2021-03-30T13:40:41Z",
+      content:
+        "Posted \r\nPayPal says it has started letting customers use bitcoin at checkout, marking the latest sign that mainstream financial firms are moving to accept cryptocurrencies. Julian Satterthwaite repo… [+3 chars]",
+    },
+  ],
+};
